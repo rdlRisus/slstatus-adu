9e065ad8c422b051fc7dcd929d1163b5293e37db
Arda “Adu” Telsiz <98750793+rdlRisus@users.noreply.github.com>
2023-09-24 22:42:27 +0300

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -65,6 +65,5 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ run_command, "|  %s%% ", "/bin/sh -c \"amixer get Master | tail -n1 | grep -Po '\\[\\K[^%]*' | head -n1\"" },	
 	{ datetime, "%s",           "%F %T" },
 };
